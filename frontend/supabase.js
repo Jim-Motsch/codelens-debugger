b63a1da499f1934b044a7c1e4d1a7981c11a3a0a
Jim_Motsch <jimmymotsch@gmail.com>
2026-04-10 19:42:06 -0500

    Add auth with Supabase

---
+++ frontend/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://psookuogjmqlvaoutdtx.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzb29rdW9nam1xbHZhb3V0ZHR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NTUxNDksImV4cCI6MjA5MTQzMTE0OX0.JOcWwyhPxKNzGKlgaUB1mU6Lc6vSHkgr_g6w54Et8OI'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
